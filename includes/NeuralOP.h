71bb9cdcefd88b7e58921e288fb1e57606df0032
HamilcarR <bensalem759@hotmail.com>
2018-09-09 15:35:08 +0200

    1st commit

--- includes/NeuralOP.h
+++ includes/NeuralOP.h
@@ -5,22 +5,29 @@
 
 
 
-enum LINKAGE {P}; 
+typedef struct NEURON; 
+typedef struct N_LAYER; 
+typedef struct NETWORK; 
+
+
 
 
 typedef struct NEURON{
 	float threshold ; 
 	float weight ; 
+	/*function pointer*/
 	void* function ; 
-	struct NEURON* next ; 
-	struct NEURON* prev ; 
+	/* NULL terminated arrays , that point to the neurons of the next and previous layer , connected to the current neuron*/
+	struct NEURON** next; 
+	struct NEURON** prev;
+
 
 	
 }NEURON;
 
 typedef struct N_LAYER{
 	int neuron_count ;
-	NEURON **layer ; 
+	NEURON **neurons ; 
 
 }N_LAYER;
 
@@ -35,8 +42,8 @@ typedef struct NETWORK{
 
 N_LAYER* create_layer(int count ) ; 
 NEURON*  create_neuron() ; 
-NETWORK* create_network(int count , N_LAYER** layers_array ,enum LINKAGE link) ; 
-void connect_layers(int count , N_LAYER** layers , enum LINKAGE link) ; 
+NETWORK* create_network(int count , N_LAYER** layers_array ) ; 
+void connect_layers_perceptron(NETWORK* network ) ; 
 
 
 
--- sources/NeuralOP.c
+++ sources/NeuralOP.c
@@ -3,15 +3,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
 NEURON* create_neuron(){
 	NEURON *neuron = malloc(sizeof(NEURON)); 
 	neuron->threshold = 0 ; 
@@ -31,20 +22,49 @@ N_LAYER* create_layer(int count){
 
 	N_LAYER* layer = malloc(sizeof(N_LAYER)); 
 	layer->neuron_count = count ; 
-	layer->layer = malloc(count*sizeof(NEURON)); 
+	layer->neurons = malloc(count*sizeof(NEURON*)); 
 	int i = 0 ; 
 	for(i ; i < count ; i++){
-		layer->layer[i] = create_neuron() ; 
+		layer->neurons[i] = create_neuron() ; 
 	}
 	return layer ; 
 
 }
 
 
-void connect_layers(int count , N_LAYER** layers ,enum LINKAGE link){
-	if (link == P) {
-
+static void connect_neurons(NEURON* neuron, N_LAYER *layer_next , N_LAYER *layer_prev) {
+	int i = 0;
+	
+	if (layer_next != NULL) {
+		if (neuron->next != NULL) {
+			free(neuron->next);
+			neuron->next = NULL;
+		}
+		NEURON** next = malloc((layer_next->neuron_count + 1) * sizeof(NEURON*));
+		for (i; i < layer_next->neuron_count; i++)
+			next[i] = layer_next->neurons[i];
+		next[i + 1] = NULL;
+		neuron->next = next;
 	}
+
+	if (layer_prev != NULL) {
+		i = 0;
+		if (neuron->prev != NULL) {
+			free(neuron->prev);
+			neuron->prev = NULL;
+		}
+		NEURON** prev = malloc((layer_prev->neuron_count + 1) * sizeof(NEURON*));
+		for (i; i < layer_prev->neuron_count; i++)
+			prev[i] = layer_prev->neurons[i];
+		prev[i + 1] = NULL;
+		neuron->prev = prev;
+	}
+	
+	
+	
+	
+	
+	
 	
 
 }
@@ -52,7 +72,43 @@ void connect_layers(int count , N_LAYER** layers ,enum LINKAGE link){
 
 
 
-NETWORK* create_network(int count , N_LAYER** layer_array ,enum LINKAGE link){
+void connect_layers_perceptron(NETWORK* network){
+	
+	int i = 0, j = 0; 
+	for (i; i < network->layer_count; i++) {
+		for (j; j < network->layers[i]->neuron_count; j++) {
+			NEURON* current = network->layers[i]->neurons[j]; 
+			if (i == 0) {
+				current->prev = NULL; 
+				if (i + 1 < network->layer_count)
+					connect_neurons(current, network->layers[i + 1], NULL); 
+
+				else
+					current->next = NULL; 
+			}
+			else if (i == network->layer_count - 1) {
+				current->next = NULL; 
+				if (i - 1 >= 0) {
+					connect_neurons(current, NULL, network->layers[i - 1]);
+				}
+				else
+					current->prev = NULL; 
+
+			}
+			else {
+				connect_neurons(current, network->layers[i + 1], network->layers[i - 1]); 
+			}
+		}
+	}
+	
+	
+
+}
+
+
+
+
+NETWORK* create_network(int count , N_LAYER** layer_array ){
 	NETWORK* network = malloc(sizeof(NETWORK)); 
 	network->layer_count = count ; 
 	network->layers = layer_array ; 
